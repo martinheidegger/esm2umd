a29ef72f85f011e7a50603174c03a627e6ac4439
Martin Heidegger <martin.heidegger@gmail.com>
2022-05-30 00:53:56 +0900

    fix: compiling to .js instead of .cjs as backup compatibility in node.js and other build tools often looks for these file. Also: when compiling package paths it is a good idea to allow relative paths, as package software doesn't support exports often.

--- bin/esm2umd.mjs
+++ bin/esm2umd.mjs
@@ -3,16 +3,16 @@ import fs from 'fs'
 import esm2umd from '../index.mjs'
 import process from 'process'
 import { pathToFileURL } from 'url'
-import { sep, join } from 'path'
+import { sep, join, relative } from 'path'
 
 if (process.argv[2] === '--help') {
   console.error("Usage: esm2umd [<ModuleName>]")
   process.exit(1)
 }
 
-async function toUMD (dirname, packageName, base) {
+async function toUMD (dirname, pkg, base) {
   const files = await fs.promises.readdir(dirname)
-  const replaceRegexp = new RegExp(`(require\\s*\\(\\s*["'])${packageName}(\\/(.+)\\.js)?(["']\\s*\\))`, 'g')
+  const moduleMatch = new RegExp(`^(${pkg.name}\\/|\\.)`)
   for (const file of files) {
     const source = new URL(file, dirname)
     const stat = await fs.promises.stat(source)
@@ -21,8 +21,7 @@ async function toUMD (dirname, packageName, base) {
         continue
       }
       await toUMD(
-        new URL(source.href + sep),
-        packageName, base === '.' ? '..' : base + sep + '..'
+        new URL(source.href + sep), pkg, base === '.' ? '..' : base + sep + '..'
       )
     }
     if (!file.endsWith('.mjs')) {
@@ -32,13 +31,13 @@ async function toUMD (dirname, packageName, base) {
       continue
     }
     const name = file.replace(/\.mjs$/, '')
-    const target = new URL(`${name}.cjs`, dirname)
+    const target = new URL(`${name}.js`, dirname)
     const esmFile = await fs.promises.readFile(source)
     let data = esm2umd(process.argv[2], esmFile, { importInterop: 'node' })
     data = data.replace(
-      replaceRegexp,
-      (found, prefix, pth, name, suffix) => {
-        return `${prefix}${pth ? `${base}/${name}.cjs` : base}${suffix}`
+      /(require\s*\(\s*["'])([^'"]+)\.mjs(["']\s*\))/g,
+      (found, prefix, module, suffix) => {
+        return moduleMatch.test(module) ? `${prefix}${module}.js${suffix}` : found
       }
     )
     await fs.promises.writeFile(target, data)
@@ -46,8 +45,10 @@ async function toUMD (dirname, packageName, base) {
 }
 
 ;(async () => {
-  const { name } = JSON.parse(await fs.promises.readFile(join(process.cwd(), 'package.json'), 'utf8'))
-  await toUMD(pathToFileURL(process.cwd() + sep), name, '.')
+  const dirname = pathToFileURL(process.cwd() + sep)
+  const jsonPath = join(dirname.pathname, 'package.json')
+  const pkg = JSON.parse(await fs.promises.readFile(jsonPath, 'utf-8'))
+  await toUMD(dirname, pkg, '.')
 })()
   .catch(err => {
     console.error(err.stack)
